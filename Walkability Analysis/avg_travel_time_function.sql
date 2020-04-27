fe38b8e8bce111498467ef92d6d19c54b01183bd
dfour001 <53315984+dfour001@users.noreply.github.com>
2020-04-27 01:05:12 -0400

    Update Walkability

--- Walkability Analysis/avg_travel_time_function.sql
+++ Walkability Analysis/avg_travel_time_function.sql
@@ -7,16 +7,26 @@ declare
     vName varchar := 'v_' || replace(ppName, ' ', '_');
 	avg double precision;
 begin
+    raise notice '====';
+	raise notice 'Looking for %', vName;
+	raise notice 'Created from %', ppName;
+	if exists (SELECT *
+                   FROM information_schema.views
+                   WHERE table_schema = 'public'
+                         AND table_name = lower(vName))
+	then
+		execute '
+			select
+				avg(n.cost)
+			from 
+				' || vName || ' n,
+				wards w
+			where w.ward_fips = ''' || ward_fips || ''' and
+			st_intersects(n.geom, w.geom)' into avg;
 
-	execute '
-		select
-			avg(n.cost)
-		from 
-			' || vName || ' n,
-			wards w
-		where w.ward_fips = ''' || ward_fips || ''' and
-		st_intersects(n.geom, w.geom)' into avg;
-	
-	return avg;
+		return avg;
+	else
+		return -1;
+	end if;
 end;
 $$ language plpgsql;
--- Walkability Analysis/walkability_analysis.sql
+++ Walkability Analysis/walkability_analysis.sql
@@ -1,28 +1,40 @@
--- Create walkability table
-drop table if exists walk_time_analysis_results;
-create table walk_time_analysis (
-	WARD_FIPS varchar(16),
-	PP_2016 varchar(225),
-	walk_time_2016 double precision,
-	PP_2020 varchar(255),
-	walk_time_2020 double precision,
-	primary key (WARD_FIPS)
-);
 
 -- Create tables holding nearest node for each polling place.
 -- One for 2018 and one for 2020.
-drop table if exists polling_place_nodes_2018
-create or replace view polling_place_nodes_2018 as (
+drop table if exists polling_place_nearest_nodes_2018;
+create table polling_place_nearest_nodes_2018 as (
 	select 
 	    pollingpla, 
 	    find_nearst_node(pollingpla) as node
 	from pp_2018general
 );
 
-drop table if exists polling_place_nodes_2020
-create or replace view polling_place_nodes_2020 as (
+
+drop table if exists polling_place_nearest_nodes_2020;
+create table polling_place_nearest_nodes_2020 as (
 	select 
 	    pollingpla, 
 	    find_nearst_node(pollingpla) as node
 	from pp_2020spring
 );
+
+
+-- Create views representing the walkability analysis for each polling place
+-- 2018:
+select generate_walking_analysis_view(node, pollingpla) from polling_place_nearest_nodes_2018;
+
+-- 2020:
+select generate_walking_analysis_view(node, pollingpla) from polling_place_nearest_nodes_2020;
+
+-- Create walkability results table
+drop table if exists walk_time_analysis_results;
+create table walk_time_analysis_results as (
+	select
+		WARD_FIPS,
+		ppname18,
+		avg_travel_time(WARD_FIPS, ppname18) as walk_time_2016,
+		ppname20,
+		avg_travel_time(WARD_FIPS, ppname20) as walk_time_2020
+	from
+		wards
+	);
