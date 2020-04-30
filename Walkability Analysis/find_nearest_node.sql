739d809474db43662897ba505d53c32b0f23a457
dfour001 <53315984+dfour001@users.noreply.github.com>
2020-04-29 21:03:12 -0400

    Add FinalProject.gdb

--- Walkability Analysis/find_nearest_node.sql
+++ Walkability Analysis/find_nearest_node.sql
@@ -2,12 +2,11 @@
 Finds the node closest to the input polling place name by measuring the distance
 to each node within a 1500' buffer of the input polling place and returning the 
 node id with the lowest distance value. */
-create or replace function find_nearst_node(ppName varchar(254)) returns int as
+create or replace function find_nearst_node(name varchar(254)) returns int as
 $$
-declare
-    name varchar(254) = ppName;
+
 begin
-	raise notice 'Finding nearest node for polling place %', ppName;
+	raise notice 'Finding nearest node for polling place %', name;
 	if exists (select * from pp_2018General where pollingpla = name)
 	then
 		--Search in 2018 PP
